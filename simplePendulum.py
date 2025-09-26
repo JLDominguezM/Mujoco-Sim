167ee3740aeb7ea39369d6a4f25d30aae1538e29
José Luis Domínguez Morales <jldm-1111@outlook.com>
2025-09-26 17:39:23 -0600

    simpleTest

---
+++ simplePendulum.py
@@ -0,0 +1,18 @@
+import mujoco
+import mujoco.viewer
+import time
+
+# Load the model from the XML file
+model = mujoco.MjModel.from_xml_path('pendulum.xml')
+data = mujoco.MjData(model)
+
+# Launch the viewer
+with mujoco.viewer.launch_passive(model, data) as viewer:
+  # Get the start time
+  start = time.time()
+  while viewer.is_running() and time.time() - start < 30:
+    # Step the simulation
+    mujoco.mj_step(model, data)
+
+    # Synchronize the viewer
+    viewer.sync()
