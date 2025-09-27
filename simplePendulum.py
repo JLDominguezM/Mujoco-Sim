f4a82a1f35dbcd64a4c8755af4d1752f1408a75f
GerardoFJ <A00838645@tec.mx>
2025-09-26 19:22:04 -0600

    Removed trash

--- simplePendulum.py
+++ simplePendulum.py
@@ -7,8 +7,7 @@ model = mujoco.MjModel.from_xml_path('myrobot.mujoco.xml')
 data = mujoco.MjData(model)
 
 # Launch the viewer
-with mujoco.viewer.launch_passive(model, data) as viewer:
+with mujoco.viewer.launch(model, data) as viewer:
     while True:
         mujoco.mj_step(model, data)
         viewer.sync()
-        time.sleep(0)  # Adjust the sleep time for desired speed
